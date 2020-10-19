e282528e79b20b42463fe5b7ab487ef99ce616f4
mvn-secomea <60345494+mvn-secomea@users.noreply.github.com>
2020-10-19 14:36:20 +0200

    Update InstallGM.sh

--- InstallGM.sh
+++ InstallGM.sh
@@ -1,20 +1,30 @@
 #!/bin/bash
 
+echo -e "Initiating Log...\c"
 touch /tmp/InstallGM.log
 apt-get update &>> /tmp/InstallGM.log
+echo -e "Done"
+
+echo -e "Installing Packages...\c"
 apt-get install -y gawk gzip zip unzip tar grep coreutils perl openssl net-tools telnet psmisc sendmail wget lib32stdc++6 &>> /tmp/InstallGM.log
+echo -e "Done"
 
+echo -e "Downloading GateManager Installer...\c"
 wget -O /tmp/GateManager.tgz https://firmware.secomea.com/download/gatemanager/8250 &>> /tmp/InstallGM.log
+echo -e "Done"
 
+echo -e "Unpacking GateManager Installer...\c"
 tar -xzvf /tmp/GateManager.tgz --directory /tmp/ &>> /tmp/InstallGM.log
+echo -e "Done"
 
-echo "DN_C='DK'
-DN_ST='Denmark'
-DN_L='Herlev'
-DN_O='Secomea'
-DN_OU='IT-Hosting'
-DN_EMAIL='hosting@secomea.com'
-DN_KW='it hosting'
+echo "Starting Installer..."
+echo "DN_C=''
+DN_ST=''
+DN_L=''
+DN_O=''
+DN_OU=''
+DN_EMAIL=''
+DN_KW=''
 CA_VERBOSE=''
 " > /tmp/local_ca.props
 
@@ -23,3 +33,4 @@ cd /tmp/
 bash RUN_GM_INSTALLER --ca /tmp/local_ca.props -y &>> /tmp/InstallGM.log
 
 gm stop &>> /tmp/InstallGM.log
+echo "Done"
